b378b63459073b95e91a0dcd09245e89b8b6f5e1
jbarbach <51690861+jbarbach@users.noreply.github.com>
2019-06-11 01:06:32 -0400

    Add files via upload

---
+++ hoops.import.fun1.R
@@ -0,0 +1,39 @@
+hoops.import.fun1 <- function(GPX)
+{ 
+	training_matrix <-read.csv("/Users/joshbarbach/Dropbox/SFL/Hoops-R-Training1.csv", header=TRUE)
+	AveGP<-mean(training_matrix[,2])
+	rowcount<-length(training_matrix[,1])
+	GPcount <- sum(training_matrix[,2] > AveGP+5)
+	return(GPcount)
+}
+
+Hoops <-read.csv(file.choose(), sep=",",header=TRUE)
+attach (Hoops)
+hist(PTS)
+plot(Hoops[,12], Hoops[,13], xlab="Points", ylab="Rebounds")
+plot(training_matrix[,12],training_matrix[,13], xlab="Points", ylab="Rebounds")
+plot(training_matrix[,12],training_matrix[,13], xlab="Points", ylab="Rebounds")
+attach(training_matrix)
+
+
+plot(Hoops[,12], Hoops[,13], xlab="Points", ylab="Rebounds", asp=2)
+abline(lm(Hoops$REB ~ Hoops$PTS. ), col = "green")
+PtsMean<-mean(Hoops$PTS.)
+PtsSd<-sd(Hoops$PTS.)
+PtsZ<-(Hoops$PTS.-PtsMean)/PtsSd
+PtsZCount<-sum(PtsZ>0)
+cor(Hoops[,12:13])
+cor(Hoops[1:141,12], Hoops[1:141,13])
+
+
+plot(Hoops[1:141,12], Hoops[1:141,13], xlab="Points", ylab="Rebounds", asp=2)
+abline(lm(Hoops$REB[1:141] ~ Hoops$PTS.[1:141] ), col = "red")
+
+
+plot(Hoops[142:350,12], Hoops[142:350,13], xlab="Points", ylab="Rebounds", color="orange", asp=2)
+abline(lm(Hoops$REB[142:350] ~ Hoops$PTS.[142:350] ), col = "purple")
+
+plot(Hoops[,12],Cars[,13], xlab="Points", ylab="Rebounds", asp=2)
+abline(lm(Hoops$REB ~ Hoops$PTS. ), col = "green")
+abline(lm(Hoops$REB[1:141] ~ Hoops$PTS.[1:141] ), col = "red")
+abline(lm(Hoops$REB[142:350] ~ Hoops$PTS.[142:350] ), col = "purple")
